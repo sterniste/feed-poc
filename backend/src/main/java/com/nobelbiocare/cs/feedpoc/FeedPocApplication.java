962da1b4808ade9c1e83d89fe852205a09852779
Stephen Stern <Stephen.Stern@nobelbiocare.com>
2018-01-11 16:57:10 +0100

    Initial commit.

---
+++ backend/src/main/java/com/nobelbiocare/cs/feedpoc/FeedPocApplication.java
@@ -0,0 +1,12 @@
+package com.nobelbiocare.cs.feedpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedPocApplication.class, args);
+	}
+}
